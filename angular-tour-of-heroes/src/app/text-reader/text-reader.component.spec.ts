c56dc9fdce9e63b84046f3a3c7076bc4afed2da4
Marsalis Sanders <mrwsanders1@gmail.com>
2019-06-03 23:51:49 -0400

    added framework for text to speech

---
+++ angular-tour-of-heroes/src/app/text-reader/text-reader.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TextReaderComponent } from './text-reader.component';
+
+describe('TextReaderComponent', () => {
+  let component: TextReaderComponent;
+  let fixture: ComponentFixture<TextReaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TextReaderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TextReaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-tour-of-heroes/src/app/text-reader/text-reader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-reader',
+  templateUrl: './text-reader.component.html',
+  styleUrls: ['./text-reader.component.css']
+})
+export class TextReaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
