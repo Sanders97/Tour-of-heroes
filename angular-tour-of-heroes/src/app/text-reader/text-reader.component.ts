4bae2d3ae01407dbb130852efdbdb7c1be2fbbdb
Marsalis Sanders <mrwsanders1@gmail.com>
2019-06-05 00:30:41 -0400

    just added some code from the example I'm using

--- angular-tour-of-heroes/src/app/text-reader/text-reader.component.ts
+++ angular-tour-of-heroes/src/app/text-reader/text-reader.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BingSpeechClient, VoiceVoiceSynthesisResponse } from 'bingspeech-api-client';
+
+// Bing Speech Key (https://www.microsoft.com/cognitive-services/en-us/subscriptions)
+let subscriptionKey = 'your_private_subscription_key';
+
+let client = new BingSpeechClient(subscriptionKey);
+client.synthesizeStream('I have a dream').then(audioStream => /* ... */);
 
 @Component({
   selector: 'app-text-reader',
