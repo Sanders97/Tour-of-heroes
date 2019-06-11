804c4fe3b2fc61ffc330d2a7c685149d91c861cb
Marsalis Sanders <mrwsanders1@gmail.com>
2019-06-10 23:46:34 -0400

    following along with Bing's quickstart

---
+++ angular-tour-of-heroes/tts.js
@@ -0,0 +1,9 @@
+// Requires request and request-promise for HTTP requests
+// e.g. npm install request request-promise
+const rp = require('request-promise');
+// Requires fs to write synthesized speech to a file
+const fs = require('fs');
+// Requires readline-sync to read command line inputs
+const readline = require('readline-sync');
+// Requires xmlbuilder to build the SSML body
+const xmlbuilder = require('xmlbuilder');
